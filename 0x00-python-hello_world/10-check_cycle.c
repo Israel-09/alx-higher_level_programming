e310e6012130c0fa5876af7ef842423ec9454601
israel-09 <israelinene91@gmail.com>
2023-03-06 11:48:01 -0800

    code fix

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks is a singly list has a cycle in it
+ * @list: a singly list
+ * Return: 1 if cycle found else 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current = list;
+
+	if (!list)
+		return (0);
+	while (current->next)
+	{
+		if (current->next == list)
+			return (1);
+		current = current->next;
+	}
+	return (0);
+}
