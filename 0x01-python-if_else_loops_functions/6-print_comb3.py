c0cb6b59237425420037ab60cf47f89d346052dd
israel-09 <israelinene91@gmail.com>
2023-03-07 06:10:33 -0800

    file upload

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range (i, 10):
+        if i == j:
+            continue
+        print("{}{}".format(i, j), end='')
+        if i < 8:
+            print(", ", end='')
+print()
