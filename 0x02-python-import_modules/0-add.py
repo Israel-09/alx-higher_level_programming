31f25a8d7346332df1e7df23cbeb7211c10f55df
israel-09 <israelinene91@gmail.com>
2023-03-09 21:32:28 +0000

    code fic

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,8 +2,7 @@
 
 if __name__ == "__main__":
     import add_0
-
     a = 1
     b = 2
     c = add_0.add(a, b)
-    print("{} + {} = {}".format(a, b, c))
+    print("{:d} + {:d} = {:d}".format(a, b, c))
