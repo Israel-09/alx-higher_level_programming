65b81cf6f636e2c76cac16fc7948d01bd3451912
israel-09 <israelinene91@gmail.com>
2023-03-10 00:47:23 +0000

    file 2 upload

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    ac = len(argv) - 1
+    print("{} argument{}".format(ac, 's.'if ac > 1 else '.'))
+    for i in range(ac + 1):
+        if i > 0:
+            print("{}: {}".format(i, argv[i]))
