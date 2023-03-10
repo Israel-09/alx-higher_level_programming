8ca66610a2c7684858bc8c7ae5e525a0cf6032b3
israel-09 <israelinene91@gmail.com>
2023-03-10 01:38:08 +0000

    code fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     from sys import argv
     ac = len(argv) - 1
     if ac == 0:
-        print("{} argument{}".format(ac, '.'))
+        print("{} arguments.".format(ac))
     else:
-        print("{} argument{}".format(ac, 's:'if ac == 1 else '.'))
+        print("{} argument{}".format(ac, 's:'if ac > 1 else ':'))
         for i in range(ac + 1):
             if i > 0:
                 print("{}: {}".format(i, argv[i]))
