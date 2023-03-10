7f61d962f5ce4abaeaca7ba6df93094155759081
israel-09 <israelinene91@gmail.com>
2023-03-10 00:56:28 +0000

    file upload

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     from sys import argv
     ac = len(argv) - 1
-    print("{} argument{}".format(ac, 's:'if ac > 1 else ':'))
+    print("{} argument{}".format(ac, 's:'if ac > 1 else '.'))
     for i in range(ac + 1):
         if i > 0:
             print("{}: {}".format(i, argv[i]))
