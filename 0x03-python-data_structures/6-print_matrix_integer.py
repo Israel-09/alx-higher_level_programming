8ab8c12c4a2746e13ae45a46f3154afe94e08fee
israel-09 <israelinene91@gmail.com>
2023-03-11 21:22:29 +0000

    codeo 6 ifx

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for s_list in matrix:
         for j in range(len(s_list)):
-            print('{}'.format(s_list[j]), end='')
+            print('{:d}'.format(s_list[j]), end='')
             if len(s_list) - 1 == j:
                 print('', end='')
             else:
