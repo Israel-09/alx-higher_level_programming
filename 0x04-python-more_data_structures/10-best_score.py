525f770e4141ae82fb9610695f1e123616fbe583
israel-09 <israelinene91@gmail.com>
2023-03-15 13:55:19 -0700

    file 10 upload

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    key_list = list(a_dictionary.keys())
+    max_score = a_dictionary.get(key_list[0])
+    for k in key_list:
+        if a_dictionary[k] > max_score:
+            max_score = a_dictionary[k]
+            max_key = k
+    return max_key
