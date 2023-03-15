b91f9714d9287bdba9b86441e35a942cfa249531
israel-09 <israelinene91@gmail.com>
2023-03-15 02:58:24 +0000

    code 12 upload

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include <Python.h>
-#include <listobject.h>
-#include <object.h>
+#include "Python.h"
 
 /**
  * print_python_list_info - prints some basic information about python list
@@ -10,16 +8,15 @@
 void print_python_list_info(PyObject *p)
 {
 	long int i, len;
-
-	len = Py_SIZE(p);
-	PyTypeObject *type;
 	PyListObject *obj = (PyListObject *)p;
+	PyListObject *item;
+	len = obj->ob_base.ob_size;
 
 	printf("[*] Size of the Python List = %li\n", len);
 	printf("[*] Allocated = %li\n", obj->allocated);
 	for (i = 0; i < len; i++)
 	{
-		type = Py_TYPE(obj->ob_item[i]);
-		printf("Element %li: %s\n", i, type->tp_name);
+		item = (PyListObject *)obj->ob_item[i];
+		printf("Element %li: %s\n", i, item->ob_base.ob_base.ob_type->tp_name);
 	}
 }
--- 0x03-python-data_structures/100-test_list.py
+++ 0x03-python-data_structures/100-test_list.py
@@ -4,7 +4,7 @@ lib = ctypes.CDLL('./libPyList.so')
 lib.print_python_list_info.argtypes = [ctypes.py_object]
 l = ['hello', 'World']
 lib.print_python_list_info(l)
-del l[1]
+l.pop()
 lib.print_python_list_info(l)
 l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
 lib.print_python_list_info(l)
