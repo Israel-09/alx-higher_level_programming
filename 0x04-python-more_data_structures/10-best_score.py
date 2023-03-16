e09b8fe9f537e229138d6f101adf5ccadcb027e6
israel-09 <israelinene91@gmail.com>
2023-03-16 01:33:49 +0000

    code 10 fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,6 +6,7 @@ def best_score(a_dictionary):
         return None
     key_list = list(a_dictionary.keys())
     max_score = a_dictionary.get(key_list[0])
+    max_key = key_list[0]
     for k in key_list:
         if not a_dictionary[k]:
             return None
