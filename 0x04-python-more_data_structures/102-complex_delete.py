80953437751283fd4f43af2b5191df3f674264ca
israel-09 <israelinene91@gmail.com>
2023-03-17 12:38:29 -0700

    code fix

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -5,7 +5,7 @@ def complex_delete(a_dictionary, value):
     if not a_dictionary:
         return None
     key_list = list(a_dictionary.keys())
-    if len(key_list) == 0 && value >= len(key_list):
+    if len(key_list) == 0 and value >= len(key_list):
         return None
     for k in key_list:
         if a_dictionary.get(k) == value:
