0ff525885de43db9f4bb711098b7bf9445f2a31c
israel-09 <israelinene91@gmail.com>
2023-03-27 16:12:09 +0000

    removed  test code

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -19,24 +19,3 @@ def safe_function(fct, *args):
         sys.stderr.write('Exception: ' + str(me) + '\n')
 
     return c
-
-def my_div(a, b):
-        return a / b
-
-
-result = safe_function(my_div, 10, 2)
-print("result of my_div: {}".format(result))
-
-result = safe_function(my_div, 10, 0)
-print("result of my_div: {}".format(result))
-
-def print_list(my_list, len):
-    i = 0
-    while i < len:
-        print(my_list[i])
-        i += 1
-    return len
-
-result = safe_function(print_list, [1, 2, 3, 4], 10)
-print("result of print_list: {}".format(result))
-
