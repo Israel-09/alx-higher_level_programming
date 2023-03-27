b59db6b5a68055762eb089416c6bb7af103a40f5
israel-09 <israelinene91@gmail.com>
2023-03-27 15:33:07 +0000

    removed test code

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
@@ -11,18 +12,3 @@ def safe_print_integer_err(value):
     except TypeError as me:
         sys.stderr.write(str(me) + '\n')
         return False
-
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
