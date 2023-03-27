a0fed68a086c0e39c91ace9bc0e9c5cf5f1cdee4
israel-09 <israelinene91@gmail.com>
2023-03-27 13:49:07 +0000

    removed test code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
-        try:
-            i = 0
-            while i < x:
-                print("{}".format(my_list[i]), end='')
-                i += 1 
-        except:
-            pass
-        print()
-        return i
+    try:
+        i = 0
+        while i < x:
+            print("{}".format(my_list[i]), end='')
+            i += 1
+    except Exception:
+        pass
+    print()
+    return i
