d3c398dc974df29206f51f5f9abedf25a5bb08b7
israel-09 <israelinene91@gmail.com>
2023-03-28 16:18:21 -0700

    code update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -63,8 +63,6 @@ class Square:
         """prints a suare using #"""
         ssize = self.__size
         space_cnt = self.__position[0]
-        if self.__position[1] > 0:
-            space_cnt = 0
         if ssize == 0:
             print()
         else:
