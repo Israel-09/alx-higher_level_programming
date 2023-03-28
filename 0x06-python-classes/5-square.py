5ec81b36e795e25dd9d56c922f3a66d32ff923dd
israel-09 <israelinene91@gmail.com>
2023-03-28 15:28:34 +0000

    removed trailing space

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -28,7 +28,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         """
         area: calculates the area of a square
