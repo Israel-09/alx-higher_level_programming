9f73360806dea77d5c9216b2a222ab78666d6781
israel-09 <israelinene91@gmail.com>
2023-03-29 15:53:12 +0000

    updates the condtions

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,11 +13,11 @@ class Square:
             position: coordinate of square
         """
         self.__size = size
-        if type(value) is not tuple or len(value) != 2:
+        if type(position) is not tuple or len(position) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0]) is not int or type(value[1]) is not int:
+        if type(position[0]) is not int or type(position[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
+        if position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
