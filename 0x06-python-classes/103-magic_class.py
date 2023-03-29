6f6d56b7f0b4b4e6baee4ea1d3b4676499b66d73
israel-09 <israelinene91@gmail.com>
2023-03-29 19:49:09 +0000

    file 103 upload

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from math import pi
+
+class MagicClass:
+    def __init__(self, radius):
+        """defines the attribute of a MagicClass object
+        
+        Args:
+            radius: radius of the object
+
+        Raise:
+            Exception TypeError: if raius is not a number
+        """
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """area defines the area occupied by a MagicClass object
+
+        Returns:
+            The area of a MagicClass Object
+        """
+        return (pi * (self.__radius ** 2))
+
+    def circumference(self):
+        """circumference defines the distance round a MagicClass object
+
+        Returns:
+            The circumference of a MagicClass Object
+        """
+        return (2 * pi * self.__radius)
