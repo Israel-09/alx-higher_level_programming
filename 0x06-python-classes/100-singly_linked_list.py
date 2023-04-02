d84335dbfbb5ccd91d546dbd6f960adf23f1e887
israel-09 <israelinene91@gmail.com>
2023-04-02 19:01:04 +0000

    singly list update

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,63 +1,36 @@
 #!/usr/bin/python3
 """module to create a linked list"""
 
-
 class Node:
-    """defines a node"""
-    def __init__(self, data, next_node=None):
-        self.__data = data
-        self.__next_node = next_node
-
-    @property
-    def data(self):
-        return self.__data
-
-    @data.setter
-    def data(self, value):
-        if type(value) is not int:
-            raise TypeError("data must be an integer")
-        self.__data = value
-
-    @property
-    def next_node(self):
-        return self.__data
-
-    @next_node.setter
-    def next_node(self, value):
-        if not isinstance(value, Node):
-            raise TypeError("next_node must be a Node object")
-        self.next_node = value
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = None
 
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
 
-    def sorted_insert(self, value):
-        new_node = Node(value)
-        if not self.head:
-            self.head = new_node
-            print(self.head.value)
-"""        current = self.head
-        while current.next_node:
-            current = current.next_node
-        current.next_node = new_node
-"""
+    def insert_node(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+        return
 
-    def __repr__(self):
-        return self.list_order()
+
+    def print_list(self):
+        current = self.head
+        print(current.data)
+        while current.next:
+            print('{:d}'.format(current.data))
+            current = current.next
+            self.head = current
+
 
 sll = SinglyLinkedList()
-sll.sorted_insert(2)
-#sll.sorted_insert(5)
-sll.sorted_insert(3)
-sll.sorted_insert(10)
-sll.sorted_insert(1)
-sll.sorted_insert(-4)
-sll.sorted_insert(-3)
-sll.sorted_insert(4)
-sll.sorted_insert(5)
-sll.sorted_insert(12)
-sll.sorted_insert(3)
-print(sll)
-
-
+sll.insert_node(5)
+sll.insert_node(5)
+sll.insert_node(9)
+sll.insert_node(1)
+sll.insert_node(4)
+sll.insert_node(4)
+sll.print_list()
