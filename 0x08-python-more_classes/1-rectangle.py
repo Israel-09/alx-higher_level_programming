5969e0cc52619d87d7ea7a6f9237076750b73ef5
israel-09 <israelinene91@gmail.com>
2023-04-04 09:56:35 +0000

    updated the condition

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,7 +11,16 @@ class Rectangle:
         Args:
             size: size of the square
         """
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
+
+        if type(height) is not int:
+            raise TypeError("width must be an integer")
+        if height < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
 
     @property
