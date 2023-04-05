f67bd1cbf190d1f277b1b636a614c33b5486646d
israel-09 <israelinene91@gmail.com>
2023-04-05 12:44:18 -0700

    task 101 solution

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,9 +6,6 @@ module contains a class locked attribute
 
 class LockedClass:
     '''class with a locked attribute first_name'''
-    def __init__(self):
-        self.__last_name = None
-
     @property
     def last_name(self):
         raise AttributeError("'LockedClass' object has no attribute 'last_name'")
