1d7aab6ff21eec6b9e750d8b42fa51aff1c4c1c2
israel-09 <israelinene91@gmail.com>
2023-05-15 14:56:49 +0000

    pycodestyle compliant

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """module on a square class"""
-
-
 Rectangle = __import__("9-rectangle").Rectangle
+
 
 class Square(Rectangle):
     '''square class that inherits from Rectangle'''
