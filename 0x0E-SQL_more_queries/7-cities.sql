2e39b037e6d5cbd78b8302b0ddd773f60a4eaedb
israel-09 <israelinene91@gmail.com>
2023-05-18 23:16:50 +0000

    task 7 solution

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,7 +7,9 @@ USE hbtn_0d_usa;
 
 CREATE TABLE if NOT EXISTS cities(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
-	state_id INT NOT NULL, FOREIGN KEY(id) REFERENCES states(id),
+	state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id),
 	name VARCHAR(256) NOT NULL
 );
+
+-- FOREIGN KEY (foreign_key_column) REFERENCES another_table (id)
 
