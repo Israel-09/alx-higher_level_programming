5f23888e20ae4d569eb36437fa486e940b82415e
israel-09 <israelinene91@gmail.com>
2023-06-14 03:07:52 +0000

    task 12 solutio

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
+'''script on pascal tringle'''
 
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
 
 def pascal_triangle(n):
+    '''
+    returns a list of list of pascal triange
+
+    Args:
+        n (int): number up to which it prints
+
+    Return:
+            a matrix of pascal triangle
+    '''
     matrix = []
     layer = []
+    if n <= 0:
+        return (matrix)
+
     for i in range(n):
         layer = layer[:]
         layer = []
         for j in range(i + 1):
             if j == i:
-                layer.append(1);
+                layer.append(1)
                 matrix.append(layer)
-                break;
+                break
             elif j == 0:
-                layer.append(1);
+                layer.append(1)
             else:
                 num1 = matrix[i - 1][j - 1]
                 num2 = matrix[i - 1][j]
                 layer.append(num1 + num2)
     return (matrix)
-
-print_triangle(pascal_triangle(8))
