4fed8e40a208bd001d879f4bf17b7e7f54035a4a
israel-09 <israelinene91@gmail.com>
2023-06-23 15:06:54 +0000

    solution to task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,11 +32,26 @@ class Base:
         '''
         from json import dumps
 
-
         if list_dictionaries:
             return dumps(list_dictionaries)
         else:
             return dumps([])
-        
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''saves list objects to a file
+        Args:
+            list_objs: is a list of instances who inherits of Base
+        '''
 
+        storage = f'{cls.__name__}.json'
+
+        list_dictionaries = []
+        for obj in list_objs:
+            obj_dict = obj.to_dictionary()
+            list_dictionaries.append(obj_dict)
+
+        str_dict = Base.to_json_string(list_dictionaries)
+
+        with open(storage, 'w', encoding='utf-8') as f:
+            f.write(str_dict)
