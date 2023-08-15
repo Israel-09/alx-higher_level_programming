8577f35c1c5347de59253340a3113606f14db044
israel-09 <israelinene91@gmail.com>
2023-08-15 15:28:56 +0000

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,8 @@
 import { argv } from 'node:process'
 
 const num = Number(argv[2]);
-if (isNaN(num)){
+
+if (isNaN(num)) {
 	console.log('Not a number');
 } else {
 	console.log(`My number: ${num}`);
