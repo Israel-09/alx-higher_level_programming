b7874b5fcd00ae02016f3bd7cffa30bed7272f34
israel-09 <israelinene91@gmail.com>
2023-08-18 23:04:52 +0000

    solution to task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- a script that lists all records of the table second_table of the database hbtn_0c_0
 
-SELECT name, score FROM second_table WHERE name is NOT NULL AND name != '' ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name is NOT NULL AND name != '' ORDER BY score DESC;
