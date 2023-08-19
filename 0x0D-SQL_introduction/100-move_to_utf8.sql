fab88aac5b38b84a0870bd776cf4d03afbabfccc
israel-09 <israelinene91@gmail.com>
2023-08-19 06:35:18 +0000

    addedsolution to task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,3 +4,4 @@
 USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8 COLLATE uf8_general_ci;
