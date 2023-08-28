44f4215c618b509c3fcede9b12dea5464bbc18c5
israel-09 <israelinene91@gmail.com>
2023-08-28 16:46:00 +0000

    solution to task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 '''fetches all rows'''
 import sys
-from model_state import Base, City
+from model_state import Base, State
+from model_city import City
 
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
@@ -10,10 +11,12 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
                            sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    
+
     session = Session(engine)
 
-    for state in session.query(City).order_by(City.id).all():
-        print("{}: {} {}".format(state.id, state.name))
+    for result in session.query(State, City).\
+            join(City, State.id == City.state_id).all():
+        print("{}: ({}) {}".format(result[0].name,
+                                   result[1].id, result[1].name))
 
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,9 +4,7 @@ from model_state import Base
 
 
 class City(Base):
-    __tablename__ = cities
-
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('state.id'))
-
