d15520b0891da7af1665efb4daadf8e434cf2b72
israel-09 <israelinene91@gmail.com>
2023-08-28 16:50:44 +0000

    pcs compliant

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,13 +3,12 @@
 """
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
+
 
 class State(Base):
     '''creates table state in the database
     '''
-
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, unique=True,
                 autoincrement=True, nullable=False)
