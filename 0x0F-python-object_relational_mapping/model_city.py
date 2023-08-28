15be17e95c0b4823b058584529e631b6a2df6327
israel-09 <israelinene91@gmail.com>
2023-08-28 16:48:02 +0000

    added documentation

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+'''creates cities tables'''
 from sqlalchemy import String, Integer, Column, ForeignKey
 from model_state import Base
 
 
 class City(Base):
+    '''creates a class for cities table'''
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
