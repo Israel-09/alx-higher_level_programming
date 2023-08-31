96d1fbd29c76d0d4c21f051e43e07f2b9f8e0f38
israel-09 <israelinene91@gmail.com>
2023-08-31 04:02:17 +0000

    task 2-4 solution

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Method
+  print () {
+    let x = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        x += 'X';
+      }
+      if (i < this.height - 1) { x += '\n'; }
+    }
+    console.log(x);
+  }
+  
+  // Method
+  rotate () {
+    const x = this.height;
+    this.height = this.width;
+    this.width = x;
+  }
+  
+  //Method
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
