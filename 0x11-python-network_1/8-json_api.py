076e0edd46dcd8bfbab930c03e30a4d676a7f834
israel-09 <israelinene91@gmail.com>
2023-09-01 22:32:13 +0000

    task 8 solution

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''post a request to a web server
+'''
+
+if __name__ == '__main__':
+    import sys
+    import requests
+    
+    value = ''
+    if len(sys.argv) > 1:
+        value = sys.argv[1]
+    request_dict = {'q': value}
+    response = requests.post("http://0.0.0.0:5000/search_user", \
+                             data=request_dict)
+    
+    try:
+        json_obj = response.json()
+        if len(json_obj) == 0:
+            print('No result')
+        else:
+            print('[{}] {}'.format(json_obj.get('id'), \
+                                   json_obj.get('name')))
+    except simplejson.errors.JSONDecodeError:
+        print('Not a valid JSON')
