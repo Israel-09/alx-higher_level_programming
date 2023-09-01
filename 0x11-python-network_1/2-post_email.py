d04fb00c8555842f67467b3126c6e7a83ba443b7
israel-09 <israelinene91@gmail.com>
2023-09-01 20:56:14 +0000

    task 2 solution

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,8 +5,10 @@ import urllib.request
 import sys
 
 if __name__ == '__main__':
-    
-    data = sys.argv[2].encode('utf-8')
+
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('utf-8')
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         page = response.read()
