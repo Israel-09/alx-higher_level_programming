8fb4891e6635c112f47601d49d7ec56d978fc59d
israel-09 <israelinene91@gmail.com>
2023-09-01 17:10:45 +0000

    fixed indentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ import urllib.request
 if __name__ == '__main__':
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
             as response:
-            page = response.read()
+        page = response.read()
 
     print('Body response:')
     print('    - type: {}'.format(type(page)))
