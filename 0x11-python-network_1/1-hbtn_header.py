02769b2a98b28063cd260fcc3dc98b7c380380af
israel-09 <israelinene91@gmail.com>
2023-09-01 17:52:12 +0000

    task 1 upload

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''sends a request to the URL and displays the
+value of the X-Request-Id'''
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        page = response.getheader('X-Request-Id')
+        print('{}'.format(page))
