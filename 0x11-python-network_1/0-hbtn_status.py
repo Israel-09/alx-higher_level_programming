00783f2a3b7ae2fc7fb15ca947544ee554bddcd4
israel-09 <israelinene91@gmail.com>
2023-09-01 17:24:28 +0000

    refactor code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         page = response.read()
-    print('Body response:')
-    print('    - type: {}'.format(type(page)))
-    print('    - content: {}'.format(page))
-    print('    - utf8 content: {}'.format(page.decode('utf-8')))
+        print('Body response:')
+        print('    - type: {}'.format(type(page)))
+        print('    - content: {}'.format(page))
+        print('    - utf8 content: {}'.format(page.decode('utf-8')))
