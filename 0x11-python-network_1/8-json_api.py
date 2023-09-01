5505771106fd17d635da055020a8a79e442b71d2
israel-09 <israelinene91@gmail.com>
2023-09-01 22:33:16 +0000

    task 8 solution

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,20 +5,20 @@
 if __name__ == '__main__':
     import sys
     import requests
-    
+
     value = ''
     if len(sys.argv) > 1:
         value = sys.argv[1]
     request_dict = {'q': value}
-    response = requests.post("http://0.0.0.0:5000/search_user", \
+    response = requests.post("http://0.0.0.0:5000/search_user",
                              data=request_dict)
-    
+
     try:
         json_obj = response.json()
         if len(json_obj) == 0:
             print('No result')
         else:
-            print('[{}] {}'.format(json_obj.get('id'), \
+            print('[{}] {}'.format(json_obj.get('id'),
                                    json_obj.get('name')))
     except simplejson.errors.JSONDecodeError:
         print('Not a valid JSON')
