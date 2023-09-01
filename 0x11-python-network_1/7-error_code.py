f21329f026d53f9d7481ffaf88715940673097f5
israel-09 <israelinene91@gmail.com>
2023-09-01 21:50:12 +0000

    solution to task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     
     response = requests.get(sys.argv[1])
     if response.status_code >= 400:
-        print("Error: {}".format(response.status_code))
+        print("Error code: {}".format(response.status_code))
     else:
         response.encoding = 'utf-8'
         print('{}'.format(response.text))
