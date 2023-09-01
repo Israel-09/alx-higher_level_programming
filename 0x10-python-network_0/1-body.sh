8a79d537b70d976c1347d0a7871fa289de989a83
israel-09 <israelinene91@gmail.com>
2023-09-01 05:20:15 +0000

    task 1 solution

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env/ bash
+# a Bash the displays the content of the body of the response of a url
+
+response=`curl -sL -w "%{http_code}" -o /dev/null $1;`
+
+if (( $response == 200 )); then
+	curl -s $1
+fi
