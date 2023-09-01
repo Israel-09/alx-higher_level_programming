3cec58dfc400c3184fbfd1e3cfe075f4f1fa32ef
israel-09 <israelinene91@gmail.com>
2023-09-01 06:42:29 +0000

    solution to 4

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-#a Bash script that takes in a URL as an argument, sends a POST
-# request to the URL.
-
-var1="email=test@gmail.com"
-var2="subject=I will always be here for PLD"
-
-curl -sX POST -d "$var1&$var2" "$1";
+#a Bash script that takes in a URL as an argument, sends a POST request to the URL.
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1";
