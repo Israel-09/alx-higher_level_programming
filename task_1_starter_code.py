d6db761d05bd7f27831780ca818568c44825343a
israel-09 <israelinene91@gmail.com>
2023-09-02 14:49:31 +0000

    added comments field to invoice

--- task_1_starter_code.py
+++ task_1_starter_code.py
@@ -23,6 +23,7 @@ class Invoice:
         self.date = datetime.now()
         self.cost = 0
         self.items = []
+        self.comments = []
 
     def add_item(self, name, price, tax):
         # python makes working with trivial data-objects quite easy
@@ -40,11 +41,23 @@ class Invoice:
         total = 0
         for item in self.items:
             price = item["price"]
-            discount_amt = discount / 100 * price
-            discount_price = price - discount_amt
-            tax = item["tax"] / 100 * discount_price
+            discount_price = price - (discount * price)
+            tax = item["tax"] * discount_price
             total += discount_price + tax
         return total
+
+    def add_comment(self, comment):
+        # adding comment to the invoice
+        if type(comment) is str:
+            self.comments.append(comment)
+
+    def get_comment(self):
+        # prints out the comment of the invoice
+        if self.comments != []:
+            comments = '\n'.join(self.comments)
+            print('{}'.format(comments))
+        else:
+            print('No added comment for invoice')
 
 
 if __name__ == '__main__':
@@ -60,5 +73,5 @@ if __name__ == '__main__':
     invoice.add_item("34 floor building", 3400, .1)
     invoice.add_item("Equipment Rental", 1000, .1)
     invoice.add_item("Fear Tax", 340, 0.0)
-    invoice_total = invoice.calculate_total(20)
+    invoice_total = invoice.calculate_total(.2)
     print(invoice_total)
