d6c44488cd6f036e08a642da69a5c92361a06864
israel-09 <israelinene91@gmail.com>
2023-09-28 01:42:17 +0000

    task 8 solution

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// prints the title of the movie for a given id
+
+const request = require('request');
+const endpoint = 'https://swapi-api.alx-tools.com/api/films/';
+const url = endpoint + process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        const characterDetails = JSON.parse(body);
+        console.log(characterDetails.name);
+      });
+    });
+  }
+});
